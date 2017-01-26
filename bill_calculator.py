53a0f2f52201dc5536371a4c4e21475fca5d5c99
Molly <molly@ravellaw.com>
2017-01-25 23:50:10 -0800

    Update and add files

--- bill_calculator.py
+++ bill_calculator.py
@@ -16,34 +16,23 @@ def main():
 
 	if choice == "1":
 		bill_amount = float(raw_input("What was the original bill amount?: "))
-		tip_percentage = float(raw_input("What is the tip percentage?: "))
+		tip_percentage = float(raw_input("How much tip would you like to leave? (percentage): "))
 		tip_amount = calculate_tip(bill_amount, tip_percentage)
-		print tip_amount
-		print calculate_total(tip_amount, bill_amount)
+		print "You should leave $%f in tip" % tip_amount
+		print "Your total is now $%f." % calculate_total(tip_amount, bill_amount)
 		split_ask = raw_input("Would you like to split the bill?: ")
 		if split_ask == "yes" or "Yes" or "Y" or "y":
 			num_people = float(raw_input("How many ways?: "))
 			bill_total = calculate_total(tip_amount, bill_amount)
-			print split_bill(bill_total, num_people)
+			print "The cost per person is $%f." % split_bill(bill_total, num_people)
 	elif choice == "2":
 		bill_amount = float(raw_input("What was the original bill amount?: "))
 		num_people = float(raw_input("How many ways?: "))
 		split_again = split_bill(bill_amount, num_people)
-		print "The cost per person is %f." % split_again
+		print "The cost per person is $%f." % split_again
 	else:
 		print "Entry not valid! Please input '1' or '2'."
 
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-
-
-
-
-
